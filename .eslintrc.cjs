90a3feb52f10a4ad2e343c7ff43dfdb8556af59a
margav <mikopos@gmail.com>
2025-05-14 19:58:57 +0300

    fix lint issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,13 +7,20 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  ignorePatterns: ["node_modules/", "dist/", "examples/"],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     'no-console': 'warn',
     'no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 }; 
